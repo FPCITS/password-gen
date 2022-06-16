14167cdf2df7d3e53c1b87d3a393365ced138a76
FPCITS <sara@freedompracticecoaching.com>
2022-06-16 11:12:49 -0500

    add variables

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 // Assignment code here
-
+var numberBase = "12345467567"
+var upperCaseBase 
+var lowerCaseBase
+var specialBase
+var passwordBase  
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(){
+  var passwordLength = prompt("Please enter your desired password length")
+  console.log(passwordLength)
+
+  if (passwordLength>128 || passwordLength<8){
+    window.alert("Your password must be between 8-128 characters long")
+    generatePassword()
+  }
+
+  var userWantsUppercase = window.confirm("Do you want uppercase letters")
+
+
+
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
